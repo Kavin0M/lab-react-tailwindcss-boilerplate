4d5c9354a5633dfd7cc528382f8e698e4d9accf8
Kavin0M <kavinsai88@gmail.com>
2023-12-08 16:39:15 +0530

    Completed

---
+++ react-tailwind/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Footer from './components/Footer'
+import MainPage from './components/MainPage'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <div>
+      <NavBar />
+      <MainPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
